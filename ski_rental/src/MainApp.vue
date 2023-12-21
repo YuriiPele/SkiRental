5b0dcf1567ceec5c216504c3bf0809c1f2e5df24
Yurii Peleshchak <yurii.peleshchak@hebronsoft.com>
2023-12-21 20:28:03 +0200

    deleted not necessary js

--- ski_rental/src/MainApp.vue
+++ ski_rental/src/MainApp.vue
@@ -1,7 +1,7 @@
 <script setup>
-import Header from "./components/main_page/header_component.vue";
+import Header from "./components/main_page/header_com.vue";
 import PriceTable from "./components/main_page/price_table/price_table.vue";
-import Footer from "./components/main_page/footer_component.vue";
+import Footer from "./components/main_page/footer_com.vue";
 </script>
 
 <template>
